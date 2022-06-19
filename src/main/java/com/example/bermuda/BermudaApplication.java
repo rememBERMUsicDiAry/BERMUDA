d93f676e4b4396132133fb711756883a603700d0
yongdori00 <yongdori98@gmail.com>
2022-06-19 22:51:35 +0900

    first commit

---
+++ src/main/java/com/example/bermuda/BermudaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bermuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BermudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BermudaApplication.class, args);
+	}
+
+}
